930de8642c0bfff7202987216136e480c8f71dee
EA Nugroho <69884456+JourneyEAN@users.noreply.github.com>
2022-11-02 15:34:52 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 
 	cout << "nama          : eko ari nugroho"<< endl;
-        cout << "nim           : 5520122022"<< endl; 
+        cout << "nim           : 081383796300"<< endl; 
         cout << "jenis kelamin : L"<< endl;
         return 0;
 }
