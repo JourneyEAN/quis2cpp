287eca84cccea93e0398cd3b4c61ec66e8434a81
EA Nugroho <69884456+JourneyEAN@users.noreply.github.com>
2022-11-02 15:27:23 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "nama          : eko ari nugroho"<< endl;
+        cout << "nim           : 5520122022"<< endl; 
+        cout << "jenis kelamin : L"<< endl;
+        return 0;
+}
